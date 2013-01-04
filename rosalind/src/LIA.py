336113557271a7944ce3227381b534fa8dc09b47
Carl Raymond <carl@orcharddev.com>
2013-01-03 19:50:40 -0500

    Working LIA

--- rosalind/src/LIA.py
+++ rosalind/src/LIA.py
@@ -5,5 +5,23 @@ Created on Dec 18, 2012
 '''
 import math
 
-print math.sqrt(0.316);
-print math.pow(7.0/8.0, 2)
+from operator import mul    # or mul=lambda x,y:x*y
+
+nCk = lambda n,k: int(round(
+    reduce(mul, (float(n-i)/(i+1) for i in range(k)), 1)))
+
+with open("rosalind_lia.txt") as spec:
+    line = spec.readline().split()
+    k = int(line[0])
+    N = int(line[1])
+
+
+print "k:", k
+print "N:", N
+
+S = pow(2, k)
+p = 0.25
+q = 1-p
+result = sum(nCk(S, i) * pow(p,i) * pow(q, S-i) for i in range(N, S+1))
+    
+print "Result:", result 
