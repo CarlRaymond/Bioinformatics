80f190902f600f4d4bd876efdc4d19bf7e36d478
Carl Raymond <carl@orcharddev.com>
2013-02-26 01:44:08 -0500

    Added working LGIS

--- rosalind/src/CORR.py
+++ rosalind/src/CORR.py
@@ -5,6 +5,7 @@ Created on Dec 23, 2012
 '''
 
 from REVC import revc
+from fasta import read
 
 # Returns true when the Hamming distance between seq1 and seq2
 # is exactly n.  Faster than computing the distance first when
@@ -17,38 +18,41 @@ def isDistance(seq1, seq2, n):
             if (dist > n): return False
     return dist == n
 
-if __name__ == '__main__':
-    with open("corr.txt") as spec:
-        rawdata = [seq.strip() for seq in spec]
 
-    # Build a dictionary with reads as keys and counts as the value.    
-    reads = {}
-    for seq in rawdata:
-        if seq in reads:
-            reads[seq] += 1
-        else:
-            reads[seq] = 1
+with open("corr.txt") as spec:
+    rawdata = [seq.strip() for name,seq in read(spec)]
     
-    print "Original data: {0} sequences.".format(len(rawdata))
-    print "Distinct reads: {0} sequences.".format(len(reads))
 
-    # Build list of sequences appearing once
-    singletons = [k for k,v in reads.iteritems() if v == 1]
-    print "Singletons: {0}".format(len(singletons))
+# Build a dictionary with reads as keys and counts as the value.    
+reads = {}
+for seq in rawdata:
+    if seq in reads:
+        reads[seq] += 1
+    else:
+        reads[seq] = 1
+ 
+print "Original data: {0} sequences.".format(len(rawdata))
+print "Distinct reads: {0} sequences.".format(len(reads))
 
-    for k,v in reads.iteritems():
-        if v > 1: print k,v
-        
-    # Build list of sequences whose revc was not read    
-    badreads = [seq for seq in singletons if revc(seq) not in reads]
-    print "Bad reads: {0}".format(len(badreads))
+# Build list of sequences appearing once
+singletons = [k for k,v in reads.iteritems() if v == 1]
+print "Singletons: {0}".format(len(singletons))
 
-    with open("rosalind_corr.out", "w+") as output:
-        for b in badreads:
-            for r in reads:
-                if isDistance(b, r, 1):
-                    output.write("- {0}->{1}\n".format(b,r))
-                    break
-                elif isDistance(b, revc(r), 1):
-                    output.write("* {0}->{1}\n".format(b, revc(r)))
-                    break
+for s in singletons: print s
+
+#for k,v in reads.iteritems():
+#    if v > 1: print k,v
+    
+# Build list of sequences whose revc was not read    
+badreads = [seq for seq in singletons if revc(seq) not in reads]
+print "Bad reads: {0}".format(len(badreads))
+
+with open("rosalind_corr.out", "w+") as output:
+    for b in badreads:
+        for r in reads:
+            if isDistance(b, r, 1):
+                output.write("- {0}->{1}\n".format(b,r))
+                break
+            elif isDistance(b, revc(r), 1):
+                output.write("* {0}->{1}\n".format(b, revc(r)))
+                break
---
+++ rosalind/src/LGIS.py
@@ -0,0 +1,82 @@
+'''
+Created on Feb 10, 2013
+
+@author: Carl Raymond
+'''
+
+with open("rosalind_lgis.txt") as spec:
+    n = int(spec.readline())
+    perm = [0] + [int(k) for k in spec.readline().split()]
+
+#n = 9
+#perm = [0] + [12, 1, 2, 13, 3, 14, 15, 4, 16, 17, 18, 19, 20, 5, 6, 7, 8, 9, 11, 10]
+#perm = [0] + [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#perm = [0] + [9, 8, 7, 6, 5, 4, 3, 2, 1]  
+#perm = [0] + [4, 3, 5, 1, 8, 2, 7, 6, 9]
+#perm = [0] + [3, 4, 5, 6, 7, 8, 9, 1, 2]
+#perm = [0] + [1,2,3,7,8,9,4,5,6]
+#perm = [0] + [6, 7, 8, 4, 5, 1, 2, 3, 9]
+
+#formatted = ", ".join(["{0:2}".format(e) for e in perm[1:]])
+#print "{0:2}: ".format(n), formatted
+
+
+# The pathinc (pathdec) array holds pairs (len, ptr) where len is the length of
+# the longest increasing (decreasing) subsequence ending at that point, and pos is
+# the index of the preceding element of the subsequence of that length. 
+pathinc = [(0,0)] * (n+1)
+pathinc[0] = (0, None)
+pathdec = [(0,0)] * (n+1)
+pathdec[0] = (0, None)
+
+# Build pathinc
+for i in xrange(1, n+1):
+    bestinc = (0, 0)
+    for j in xrange(i):
+        if (perm[i] > perm[j] and pathinc[j][0] >= bestinc[0]):
+            bestinc = (pathinc[j][0]+1, j)
+    pathinc[i] = bestinc
+
+maxposinc = 0
+for i in xrange(n+1):
+    if pathinc[i][0] > pathinc[maxposinc][0]:
+        maxposinc = i
+
+# Form the longest increasing subsequence by working backward
+increasing = [perm[maxposinc]]
+nextpos = pathinc[maxposinc][1]
+while nextpos:
+    increasing.append(perm[nextpos])
+    nextpos = pathinc[nextpos][1]
+increasing.reverse()
+#print "Increasing:"
+for x in increasing: print x,
+print
+
+# Build pathdec
+perm[0] = n+1
+for i in xrange(1, n+1):
+    bestdec = (0, 0)
+    for j in xrange(i):
+        if (perm[i] < perm[j] and pathdec[j][0] >= bestdec[0]):
+            bestdec = (pathdec[j][0]+1, j)
+    pathdec[i] = bestdec
+    
+
+# Find maximum length and position of increasing and decresing sequences
+maxposdec = 0
+for i in xrange(n+1):
+    if pathdec[i][0] > pathdec[maxposdec][0]:
+        maxposdec = i
+
+
+decreasing = [perm[maxposdec]]
+nextpos = pathdec[maxposdec][1]
+while nextpos:
+    decreasing.append(perm[nextpos])
+    nextpos = pathdec[nextpos][1]
+decreasing.reverse()
+#print "Decreasing:"
+for x in decreasing: print x,
+print
+
