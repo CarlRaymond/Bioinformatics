87cb4b07878a83c10dd792c0ead66194795131cc
Carl Raymond <carl@orcharddev.com>
2015-12-06 22:54:21 -0500

    Working LEXV.

---
+++ rosalind/src/CAT2.py
@@ -0,0 +1,18 @@
+'''
+Created Nov. 29, 2015
+@author Carl J. Raymond
+'''
+
+#s = 'AUAUGCGC'
+s = 'GGAUUAACCGGCGUGUACGCAGCGUAGCCUGCCGAUCCGGAUGCAUGCAUAUUAAAAUUUGAUCCUUGCAGAUCAUUUGCAUUAAAGCAGUUGCCUACGGUAUAAAGCCGCUUGCAUGCGCGCCCGGGGCCAAUUGGUAUACUAUCGACUAUGCACGAUCGUAACCCGGGGCGCAUUUUCGGCGCAAGGCCCAAUUGACGUUAAAUGCGCGGCCUGCAUAUAUAGCAGUUAACGCCACUAUCGAGGCUGCGCCGGGCCGCGCAUGCGCGCGCGCAU'
+
+c = {'':1, 'A':0, 'C':0, 'G':0, 'U':0, 'AA':0, 'AC':0, 'AG':0, 'AU':1, 'CA':0, 'CC':0, 
+    'CG':1, 'CU':0, 'GA':0, 'GC':1, 'GG':0, 'GU':0, 'UA':1, 'UC':0, 'UG':0, 'UU':0}
+
+def cat(s):
+    if s not in c:
+        c[s] = sum([cat(s[1:k]) * c[s[0]+s[k]] * cat(s[k+1:]) for k in range(1, len(s))])
+    return c[s]
+	
+print s
+print cat(s)
